09b7263492e04c983e3fe0d76dfbdc363d448ad6
saeedix <moonice73@gmail.com>
2025-04-24 20:12:15 +0330

    add DMT experience track

---
+++ tracks/DMT_experience_v1/main.rb
@@ -0,0 +1,99 @@
+# Sonic Pi: Neurosonic DMT Experience v2.3
+# بهینه‌سازی سطح صدا با حفظ کیفیت
+
+use_bpm 60
+use_debug false
+
+# 1. تنظیمات کوک
+use_tuning :equal, 432
+
+# 2. پارامترهای پایه با افزایش دامنه
+global_binaural_base = 200
+theta_diff = 7.0
+random_seed = 1
+
+# 3. افکت کلی با تقویت کنترل شده
+main_fx = with_fx :level, amp: 1.5 do # افزایش از 1.0 به 1.5
+  with_fx :reverb, mix: 0.3, room: 0.7 do # کاهش کمی ریورب برای وضوح بیشتر
+    
+    # A. بایناورال تتا (تقویت شده)
+    in_thread do
+      loop do
+        play global_binaural_base - (theta_diff/2), amp: 0.15, pan: -0.8, release: 6 # +87.5%
+        play global_binaural_base + (theta_diff/2), amp: 0.15, pan: 0.8, release: 6
+        sleep 6
+      end
+    end
+    
+    # B. پد کرال تقویت شده
+    in_thread do
+      use_synth :dark_ambience
+      loop do
+        play_chord chord(:D4, :add9), release: 16, amp: 0.4 # +33%
+        sleep 16
+        play_chord chord(:G4, :major7), release: 12, amp: 0.35 # +25%
+        sleep 12
+        play_chord chord(:A4, :sus4), release: 10, amp: 0.32 # +23%
+        sleep 10
+      end
+    end
+    
+    # C. آرپژ با دامنه بالاتر
+    in_thread do
+      use_synth :pluck
+      scale_notes = scale(:D4, :dorian, num_octaves: 2)
+      loop do
+        16.times do
+          play scale_notes.choose, release: rrand(0.2,0.4), amp: 0.3, pan: rrand(-0.5,0.5) # +50%
+          sleep choose([0.25,0.5,0.75])
+        end
+        scale_notes = scale(choose([:D, :E, :F, :G, :A]), :dorian, num_octaves: 2)
+      end
+    end
+    
+    # D. ساب بیس قوی‌تر
+    in_thread do
+      use_synth :fm
+      loop do
+        play :D2, release: 1, amp: 0.4, cutoff: 70 # +33% + فیلتر
+        sleep 1
+      end
+    end
+    
+    # E. زمینه‌های عصبی تقویت شده
+    in_thread do
+      use_random_seed random_seed
+      use_synth :hollow
+      loop do
+        n = scale(:e3, :hirajoshi, num_octaves: 2).choose
+        play n, amp: 0.15, release: 0.4, pan: rrand(-0.5,0.5) # +87.5%
+        sleep choose([0.25,0.5,0.75,1])
+        random_seed += 1
+      end
+    end
+    
+    # F. اکو با حجم بیشتر
+    in_thread do
+      loop do
+        with_fx :echo, phase: 1.618 * 0.5, mix: 0.25 do
+          sample :ambi_glass_rub, rate: choose([0.75,1]), amp: 0.2 # +100%
+        end
+        sleep 1
+      end
+    end
+    
+  end
+end
+
+# 4. فیدآوت با دامنه اولیه بالاتر
+in_thread do
+  sleep 150
+  30.times do |i|
+    set :main_amp, 1.5 * (1 - (i / 30.0))
+    sleep 0.5
+  end
+end
+
+
+
+sleep 180
